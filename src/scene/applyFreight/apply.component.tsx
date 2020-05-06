9f43cea54ee52f25a45697746fb353ec627940cd
Changhyen Kim <chang9403@ajou.ac.kr>
2020-05-06 18:08:00 +0900

    Applied daum-postcode API

--- src/scene/applyFreight/apply.component.tsx
+++ src/scene/applyFreight/apply.component.tsx
@@ -13,6 +13,9 @@ import {
   LayoutElement, TopNavigation,
   Button, Layout, Select, Input,
 } from '@ui-kitten/components';
+
+import Modal from 'react-native-modal'
+import Postcode from 'react-native-daum-postcode'
 import { ApplyScreenProps } from '../../navigation/apply.navigator';
 import { AppRoute } from '../../navigation/app-routes';
 
@@ -35,6 +38,15 @@ const freightType = [
   { text: '파레트'},
 ];
 
+const freightStartDate = [
+  { text: '당일 상차(당상)'},
+  { text: '내일 상차(내상)'},
+]
+
+const freightEndDate = [
+  { text: '당일 도착(당착)'},
+  { text: '내일 도착'},
+]
 export const ApplyScreen = (props: ApplyScreenProps): LayoutElement => {
 
   const [selectedCarSizeOption, setSelectedCarSizeOption] = React.useState(null);
@@ -51,6 +63,18 @@ export const ApplyScreen = (props: ApplyScreenProps): LayoutElement => {
   const [distValue, setDistValue] = React.useState('');
   const [expenseValue, setExpenseValue] = React.useState('');
 
+  //State를 이용하여 Modal을 제어함
+  const [modalStartAddrVisible, setmodalStartAddrVisible] = useState<boolean>(false);
+  const [modalEndAddrVisible, setmodalEndAddrVisible] = useState<boolean>(false);
+
+  //Output을 State로 받아서 화면에 표출하거나 정보 값으로 활용
+  const [modalStartAddrOutput, setmodalStartAddrOutput] = useState<string>("주소를 선택/변경해주세요");
+  const [modalEndAddrOutput, setmodalEndAddrOutput] = useState<string>("주소를 선택/변경해주세요");
+
+  // 당상/당착/내상/내착
+  const [selectedStartDateOption, setSelectedStartDateOption] = React.useState(null);
+  const [selectedEndDateOption, setSelectedEndDateOption] = React.useState(null);
+
   return (
     <React.Fragment>
       <SafeAreaView style={{flex: 0, backgroundColor: 'white'}} />
@@ -62,22 +86,48 @@ export const ApplyScreen = (props: ApplyScreenProps): LayoutElement => {
         <View style={styles.infoContainer}>
           <Text style={styles.subTitle}>위치 정보</Text>
           <View style={styles.rowContainer}>
-            <Text style={styles.infoTitle}>상차지 : 서울시 서초구 반포동 55-2</Text>
-            <Button appearance='outline' size='small'>변경</Button>
+            <Text style={styles.infoTitle}>상차지 : {modalStartAddrOutput}</Text>
+            <Button 
+              appearance='outline' 
+              size='small'
+              onPress={() => {
+                setmodalStartAddrVisible(true);
+              }}
+            >변경</Button>
           </View>
           <View style={styles.rowContainerWithLine}>
-            <Text style={styles.infoTitle}>시간 : </Text>
-            <Button appearance='outline' size='small'>변경</Button>
+            <Text style={styles.infoTitle}>상차일 : </Text>
+            <Layout style={styles.selectContainer}>
+              <Select
+                placeholder='선택'
+                basic
+                data={freightStartDate}
+                selectedOption={selectedStartDateOption}
+                onSelect={setSelectedStartDateOption}
+              />
+            </Layout>
           </View>
           <View style={styles.rowContainer}>
-            <Text style={styles.infoTitle}>하차지 : 서울시 서초구 반포동 55-2</Text>
-            <Button appearance='outline' size='small'>변경</Button>
+            <Text style={styles.infoTitle}>하차지 : {modalEndAddrOutput}</Text>
+            <Button 
+              appearance='outline'
+              size='small'
+              onPress={() => {
+                setmodalEndAddrVisible(true);
+              }}
+            >변경</Button>
           </View>
           <View style={styles.rowContainer}>
-            <Text style={styles.infoTitle}>시간 : </Text>
-            <Text style={styles.infoTitle}>06시 00분</Text>
-            <Text style={styles.infoTitle}>유형 : </Text>
-            <Text style={styles.infoTitle}>내착</Text>
+            <Text style={styles.infoTitle}>하차일 : </Text>
+            <Layout style={styles.selectContainer}>
+              <Select
+                placeholder='선택'
+                basic
+                data={freightEndDate}
+                selectedOption={selectedEndDateOption}
+                onSelect={setSelectedEndDateOption}
+              />
+            </Layout>
           </View>
         </View>
 
@@ -87,6 +137,7 @@ export const ApplyScreen = (props: ApplyScreenProps): LayoutElement => {
             <Text style={styles.infoTitle}>차량 정보 : </Text>
             <Layout style={styles.selectContainer}>
               <Select
+                placeholder='톤수'
                 data={carSize}
                 selectedOption={selectedCarSizeOption}
                 onSelect={setSelectedCarSizeOption}
@@ -94,6 +145,7 @@ export const ApplyScreen = (props: ApplyScreenProps): LayoutElement => {
             </Layout>
             <Layout style={styles.selectContainer}>
               <Select
+                placeholder='타입'
                 data={carType}
                 selectedOption={selectedCarTypeOption}
                 onSelect={setSelectedCarTypeOption}
@@ -104,6 +156,7 @@ export const ApplyScreen = (props: ApplyScreenProps): LayoutElement => {
             <Text style={styles.infoTitle}>운행 방식 : </Text>
             <Layout style={styles.selectContainer}>
               <Select
+                placeholder='독차/혼적 여부 선택'
                 data={driveType}
                 selectedOption={selectedDriveOption}
                 onSelect={setSelectedDriveOption}
@@ -124,12 +177,13 @@ export const ApplyScreen = (props: ApplyScreenProps): LayoutElement => {
           <View style={styles.rowContainer}>
             <Text style={styles.infoTitle}>화물 크기 : </Text>
             <Input
-              placeholder=''
+              placeholder='숫자로 입력'
               value={volumeValue}
               onChangeText={nextValue => setVolumeValue(nextValue)}
             />
             <Layout style={styles.selectContainer}>
               <Select
+                placeholder='단위'
                 data={freightType}
                 selectedOption={selectedFreightTypeOption}
                 onSelect={setSelectedFreightTypeOption}
@@ -197,6 +251,60 @@ export const ApplyScreen = (props: ApplyScreenProps): LayoutElement => {
           </View>
         </View>
 
+        <Modal
+          //isVisible Props에 State 값을 물려주어 On/off control
+          isVisible={modalStartAddrVisible}
+          //아이폰에서 모달창 동작시 깜박임이 있었는데, useNativeDriver Props를 True로 주니 해결되었다.
+          useNativeDriver={true}
+          hideModalContentWhileAnimating={true}
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <SafeAreaView style={{flex: 0, backgroundColor: 'white'}} />
+          <View>
+            <Postcode
+              style={{width: 350, height: 600}}
+              jsOptions={{ animated: true }}
+              onSelected={(startAddrResult) => {
+                setmodalStartAddrOutput(JSON.stringify(startAddrResult.address).replace(/\"/gi, ""))
+                setmodalStartAddrVisible(false)
+              }}
+            />
+            <Button
+              onPress={() => {
+                setmodalStartAddrVisible(false)
+              }}>
+            뒤로 돌아가기</Button>
+          </View>
+        </Modal>
+
+        <Modal
+          //isVisible Props에 State 값을 물려주어 On/off control
+          isVisible={modalEndAddrVisible}
+          //아이폰에서 모달창 동작시 깜박임이 있었는데, useNativeDriver Props를 True로 주니 해결되었다.
+          useNativeDriver={true}
+          hideModalContentWhileAnimating={true}
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <SafeAreaView style={{flex: 0, backgroundColor: 'white'}} />
+          <View>
+            <Postcode
+              style={{width: 350, height: 600}}
+              jsOptions={{ animated: true }}
+              onSelected={(endAddrResult) => {
+                setmodalEndAddrOutput(JSON.stringify(endAddrResult.address).replace(/\"/gi, ""))
+                setmodalEndAddrVisible(false)
+              }}
+            />
+            <Button
+              onPress={() => {
+                setmodalEndAddrVisible(false)
+              }}>
+            뒤로 돌아가기</Button>
+          </View>
+        </Modal>
+
+        
+
       </ScrollView>
     </React.Fragment>
   );
@@ -262,5 +370,13 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     fontStyle: 'normal',
-  },    
+  },
+
+  lineStyle:{
+    borderWidth: 0.5,
+    borderColor:'black',
+    margin:10,
+  },
+
+  
 });
