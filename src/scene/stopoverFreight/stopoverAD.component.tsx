869b520255df1a149c715923211ac1f45dca9c33
limkim4233 <limkim4233@gmail.com>
2020-05-30 09:02:53 +0900

    Added fetch function for stoppover candidates

--- src/scene/stopoverFreight/stopoverAD.component.tsx
+++ src/scene/stopoverFreight/stopoverAD.component.tsx
@@ -1,54 +1,95 @@
 import React, {useState} from 'react';
-import {
-  StyleSheet,
-  View,
-  Image
-} from 'react-native';
-import {
-  Button,
-  LayoutElement,
-} from '@ui-kitten/components';
+import {StyleSheet, View, Image} from 'react-native';
+import {Button, LayoutElement} from '@ui-kitten/components';
 import {StopoverADScreenProps} from '../../navigation/home.navigator';
 import {AppRoute} from '../../navigation/app-routes';
+import AsyncStorage from '@react-native-community/async-storage';
 
+export class StopoverADScreen extends React.Component<StopoverADScreenProps> {
+  constructor(props) {
+    super(props);
+    // this.getFreightID;
+  }
 
-export const StopoverADScreen = (props: StopoverADScreenProps): LayoutElement => {
-
-  const ApplyButton = () => {
-    props.navigation.navigate(AppRoute.STOPOVER);
+  componentDidMount = async () => {
+    try {
+      const value = await AsyncStorage.getItem('FreightID');
+      if (value !== null) {
+        var freightID = value + '';
+        //const freightID = {FrightID: value + ''};
+        console.log('value is' + value);
+        var url = '49.50.172.39:8000/stopover?freightId=' + value;
+        console.log(url);
+        var data = fetch(
+          'http://49.50.172.39:8000/stopover?freightId=' + value,
+          {},
+        )
+          .then((response) => response.json())
+          .then((response) => {
+            console.log(response);
+            // AsyncStorage.setItem('stopover1', response[0].id);
+            // AsyncStorage.setItem('stopover2', response[1].id);
+            // AsyncStorage.setItem('stopover3', response[2].id);
+          });
+      }
+    } catch (error) {}
   };
 
-  const DenyButton = () => {
-    props.navigation.navigate(AppRoute.HOME);
+  ApplyButton = () => {
+    this.props.navigation.navigate(AppRoute.STOPOVER);
   };
 
-  return (
-    <React.Fragment>
-      <View style={{backgroundColor: 'white', alignItems: 'center', flex: 3}}>
-        <Image style={styles.adImage} source={require('../../assets/StopoverAD.jpg')}/>
-      </View>
+  DenyButton = () => {
+    this.props.navigation.navigate(AppRoute.HOME);
+  };
 
-      <View style={{flexDirection: 'row', justifyContent: 'center', backgroundColor: 'white', flex: 1}}>
-        <View style={{flex: 1}}> 
-          <Button style={{margin: 8}} onPress={ApplyButton} status='success'>수 락</Button>
+  render() {
+    return (
+      <React.Fragment>
+        <View style={{backgroundColor: 'white', alignItems: 'center', flex: 3}}>
+          <Image
+            style={styles.adImage}
+            source={require('../../assets/StopoverAD.jpg')}
+          />
         </View>
-        <View style={{flex: 1}}>
-          <Button style={{margin: 8}} onPress={DenyButton} status='danger'>거 부</Button>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            backgroundColor: 'white',
+            flex: 1,
+          }}>
+          <View style={{flex: 1}}>
+            <Button
+              style={{margin: 8}}
+              onPress={this.ApplyButton}
+              status="success">
+              수 락
+            </Button>
+          </View>
+          <View style={{flex: 1}}>
+            <Button
+              style={{margin: 8}}
+              onPress={this.DenyButton}
+              status="danger">
+              거 부
+            </Button>
+          </View>
         </View>
-      </View>
-    </React.Fragment>
-  ); 
-  
-};
+      </React.Fragment>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   adImage: {
     width: '100%',
     height: '100%',
     alignItems: 'center',
-    borderRadius: 15
+    borderRadius: 15,
   },
-  icon2:{
+  icon2: {
     justifyContent: 'center',
     width: 20,
     height: 15,
