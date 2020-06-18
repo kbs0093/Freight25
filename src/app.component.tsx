7a4217849f6cff0bfd53039e3753bfb2467e6698
Kobeomseok <power0093>
2020-06-18 22:23:39 +0900

    백그라운드 서비스 콘솔 메시지까지 구현

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { YellowBox, ImageBackground, StyleSheet, } from 'react-native';
+import { YellowBox, ImageBackground, StyleSheet, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import {
@@ -15,8 +15,46 @@ import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { AppNavigator } from './navigation/app.navigation';
 import { AppRoute } from './navigation/app-routes';
 import { ThemeContext } from '../src/component/theme-context';
+import BackgroundService from 'react-native-background-actions';
+import BackgroundJob from 'react-native-background-actions';
 
 const themes = { light, dark };
+
+
+
+const sleep = time => new Promise(resolve => setTimeout(() => resolve(), time));
+
+const taskRandom = async taskData => {
+  if (Platform.OS === 'ios') {
+    console.warn(
+      'This task will not keep your app alive in the background by itself, use other library like react-native-track-player that use audio,',
+      'geolocalization, etc. to keep your app alive in the background while you excute the JS from this library.',
+    );
+  }
+  await new Promise(async resolve => {
+    // For loop with a delay
+    const {delay} = taskData;
+    for (let i = 0; BackgroundJob.isRunning(); i++) {
+      console.log('Runned -> ', i);
+      await sleep(delay);
+    }
+  });
+};
+
+
+const options = {
+    taskName: '위치 추적 시스템',
+    taskTitle: '운전자의 위치를 추적합니다',
+    taskDesc: '화물 위치 추적중...',
+    taskIcon: {
+        name: 'ic_launcher',
+        type: 'mipmap',
+    },
+    color: '#ffffff',
+    parameters: {
+        delay: 10000,
+    },
+};
 
 export default (): React.ReactFragment => {
   const [theme, setTheme] = React.useState('dark');
@@ -26,6 +64,26 @@ export default (): React.ReactFragment => {
     const nextTheme = theme === 'light' ? 'dark' : 'light';
     setTheme(nextTheme);
   };
+
+  const toggleBackground = async () => {
+    var playing = BackgroundJob.isRunning();
+
+    playing = !playing;
+    if (playing) {
+      try {
+        console.log('Trying to start background service');
+        await BackgroundJob.start(taskRandom, options);
+        console.log('Successful start!');
+      } catch (e) {
+        console.log('Error', e);
+      }
+    } else {
+      console.log('Stop background service');
+      await BackgroundJob.stop();
+    }
+  };
+
+  toggleBackground();
 
   return (
     <React.Fragment>
