07007bc91a3629e1d6e06e7985e93e9082eb5f0d
Changhyen Kim <chang9403@ajou.ac.kr>
2020-06-24 23:37:30 +0900

    Heads-up messaging setting for android (channel)

--- android/app/src/main/java/com/freight25/MainActivity.java
+++ android/app/src/main/java/com/freight25/MainActivity.java
@@ -1,6 +1,11 @@
 package com.freight25;
 
 import com.facebook.react.ReactActivity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +17,21 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "Freight25";
   }
+
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {    
+      NotificationChannel notificationChannel = new NotificationChannel("500", "MainChannel", NotificationManager.IMPORTANCE_HIGH);
+      notificationChannel.setShowBadge(true);
+      notificationChannel.setDescription("Test Notifications");
+      notificationChannel.enableVibration(true);
+      notificationChannel.enableLights(true);
+      notificationChannel.setVibrationPattern(new long[]{400, 200, 400});
+      //notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+      NotificationManager manager = getSystemService(NotificationManager.class);
+      manager.createNotificationChannel(notificationChannel);
+    }
+  }
+
 }
